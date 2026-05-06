313565b37c45af58d81262846ba5a6c89628335a
Ahmad Ghaffar <ahmadghaffar29600@gmail.com>
2026-05-06 21:30:14 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Number Guessing Game
+
+import random
+random_number = random.randint(1, 100)
+
+guess = 0
+
+while guess != random_number:
+    guess = int(input("Guess the number: "))
+    if guess < random_number:
+        print("Too Low! Guess Again...")
+    elif guess > random_number:
+        print("Too High! Guess Again...")
+    else:
+        print("Congratulations! You have guessed correct number.")
